11d44e753ea0b1aa5eeda2f9704d80fe85fcb6f4
Nathan Carver <nathantc@gmail.com>
2013-07-26 15:27:32 -0400

    please

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 (function() {
 
-    var module = angular.module('ContactBuckets', []);
+    var module = angular.module('ContactBuckets', ['ContactDataModule']);
+
+    module.controller('ContactController', ['$scope', 'ContactDataService',
+            function($scope, contactDataService) {
+
+        $scope.contacts = contactDataService.getAll();
+    }])
 
 })();
--- js/app.test.js
+++ js/app.test.js
@@ -1,8 +1,50 @@
 describe('ContactBuckets', function() {
 
-    beforeEach(function() {
-        angular.mock.module('ContactBuckets');
+    describe('Module configuration', function() {
+
+        var module, dependencies;
+        function hasModule(m) {
+            return dependencies.indexOf(m) >= 0;
+        }
+
+        beforeEach(function() {
+            module = angular.module('ContactBuckets');
+            dependencies = module.value('appName').requires;
+        });
+
+        it('should be registered', function() {
+            expect(module).toBeDefined();
+        });
+
+        it('should have ContactDataModule as a dependency', function() {
+            expect(hasModule('ContactDataModule')).toEqual(true);
+        });
     });
 
+    describe('ContactController', function() {
 
+        describe('Initializing the controller', function() {
+
+            var contacts, _scope, _controller, contactDataService;
+            contacts = [{ name: 'Joe' }];
+
+            beforeEach(function() {
+                angular.mock.module('ContactBuckets');
+                inject(function($rootScope, $controller, ContactDataService) {
+                    _controller = $controller;
+                    contactDataService = ContactDataService;
+                    _scope = $rootScope.$new();
+                });
+
+                spyOn(contactDataService, 'getAll').andReturn(contacts);
+                _controller('ContactController', {$scope: _scope, ContactDataService: contactDataService});
+            })
+
+            it('retrieves contact data', function() {
+                expect(_scope.contacts).toBe(contacts);
+            })
+
+
+        })
+    });
 });
